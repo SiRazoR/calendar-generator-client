ac09a9494f1334b5657648d7e64c2b567098eee1
SiRazoR <torecz@o2.pl>
2019-10-06 22:06:19 +0200

    &Added dialog that prevents page usage if API is offline

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import LecturesSelection from './screens/LecturesSelection'
 import Header from './components/navigation/Header'
 import Footer from './components/navigation/Footer'
 import axios from "axios";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Dialog from "@material-ui/core/Dialog";
+import Draggable from "react-draggable";
+import Paper from "@material-ui/core/Paper";
 
 
 export default class App extends React.Component {
@@ -14,6 +20,8 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            apiIsRunning: false,
+            dialogOpen: true,
             activeStep: 0,
             steps: ['Select group', 'Select lectures', 'Finish'],
             selectedGroups: [
@@ -28,11 +36,22 @@ export default class App extends React.Component {
             generatedLink: ""
         };
 
+
+    }
+
+    componentDidMount() {
         console.log("Waking up API");
-        axios.get('https://uek-calendar-generator.herokuapp.com/').catch(
+        axios.get('https://uek-calendar-generator.herokuapp.com/wake')
+            .then(response => {
+                console.log("response status" + response.status);
+                if (response.status === 200) {
+                    console.log("Got API response! We are ready to go!");
+                    this.setState({apiIsRunning: true});
+                }
+            }).catch(
             function (error) {
                 console.log(error)
-            })
+            });
     }
 
     setActiveStep = (value) => {
@@ -60,12 +79,44 @@ export default class App extends React.Component {
         this.setState({generatedLink: value});
     };
 
+    handleClose = () => {
+        this.setState({open: false})
+    };
+
+    paperComponent = (props) => {
+        return (
+            <Draggable cancel={'[class*="MuiDialogContent-root"]'}>
+                <Paper {...props} />
+            </Draggable>
+        );
+    };
+
     render() {
         return (
             <div style={styles.root}>
+
+                {this.state.apiIsRunning === false &&
+                <Dialog
+                    open={this.state.dialogOpen}
+                    onClose={this.handleClose}
+                    PaperComponent={this.paperComponent}
+                    aria-labelledby="draggable-dialog-title"
+                >
+                    <DialogTitle style={{cursor: 'move', alignSelf: "center"}} id="draggable-dialog-title">
+                        Schedule API is starting
+                    </DialogTitle>
+                    <DialogContent style={{textAlign: "center"}}>
+                        <DialogContentText>
+                            Please be patient, calendar API is deployed on free server so it needs to be waken up.
+                            <br/><br/>
+                            This window will disappear in a moment.
+                        </DialogContentText>
+                    </DialogContent>
+                </Dialog>
+                }
                 <div style={styles.card}>
                     <Header style={styles.header} activeStep={this.state.activeStep} steps={this.state.steps}/>
-                    <div style={styles.content}>
+                    <div>
                         {this.state.activeStep === 0 && <Home setSelectedGroups={this.setSelectedGroups}/>}
                         {this.state.activeStep === 1 && <LecturesSelection getSelectedGroups={this.state.selectedGroups}
                                                                            setStepTwoCompleted={this.setStepTwoCompleted}
@@ -96,23 +147,13 @@ const styles = {
     card: {
         position: "relative",
         marginLeft: "10px",
-        //marginTop: 10,
-        //marginBottom: 10,
         padding: 5,
         width: "1300px",
-        // maxWidth: 1200
     },
     header: {
         width: "100%",
     },
-    content: {
-       // height:"60%"
-    },
     footer: {
         marginTop: "50px"
-        // position: "absolute",
-        // width: "100%",
-        // bottom:10,
-        // left:0
     }
 };
