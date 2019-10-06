f476bd147ac08d3bae044ad4dd03c965cb07dbbc
SiRazoR <torecz@o2.pl>
2019-10-06 18:18:07 +0200

    & code formatting

--- src/screens/Finish.js
+++ src/screens/Finish.js
@@ -39,51 +39,52 @@ export default class Finish extends React.Component {
 
     render() {
         return (
-                <Paper style = {styles.paper}>
-                    <div style={styles.text}>
-                        <h1>UEK Calendar Generator <img src={logo} alt="Logo" style={styles.logo}/></h1>
-                    </div>
-                    <div style={styles.content}>
-                        <h2>Here is your generated calendar URL. <br/> Copy and paste it into your google calendar.</h2>
-                        <TextField
-                            id="outlined"
-                            style={{margin: 8}}
-                            value={this.props.getGeneratedLink}
-                            variant="outlined"
-                        />
-                        <CopyToClipboard text={this.props.getGeneratedLink}>
-                            <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-                                copy to clipboard
-                            </Button>
-                        </CopyToClipboard>
-
-                        <Button variant="outlined" color="primary"
-                                href={'https://calendar.google.com/calendar/r/settings/addbyurl'}>
-                            open google calendar
+            <Paper style={styles.paper}>
+                <div style={styles.text}>
+                    <h1>UEK Calendar Generator <img src={logo} alt="Logo" style={styles.logo}/></h1>
+                </div>
+                <div style={styles.content}>
+                    <h2>Here is your generated calendar URL. <br/> Copy and paste it into your google calendar.</h2>
+                    <TextField
+                        id="outlined"
+                        style={{margin: 8}}
+                        value={this.props.getGeneratedLink}
+                        variant="outlined"
+                    />
+                    <CopyToClipboard text={this.props.getGeneratedLink}>
+                        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+                            copy to clipboard
                         </Button>
-                    </div>
-                    <Dialog
-                        open={this.state.open}
-                        onClose={this.handleClose}
-                        PaperComponent={this.paperComponent}
-                        aria-labelledby="draggable-dialog-title"
-                    >
-                        <DialogTitle style={{cursor: 'move',alignSelf:"center"}} id="draggable-dialog-title">
-                            Data successfully copied
-                        </DialogTitle>
-                        <DialogContent style={{textAlign:"center"}}>
-                            <DialogContentText>
-                                Now you are ready to paste copied URL into your google calendar.
-                                <br/><br/>Click "Open google calendar" button below to be redirected directly into the settings.
-                            </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={this.handleClose} color="primary">
-                                OK
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-                </Paper>
+                    </CopyToClipboard>
+
+                    <Button variant="outlined" color="primary"
+                            href={'https://calendar.google.com/calendar/r/settings/addbyurl'}>
+                        open google calendar
+                    </Button>
+                </div>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    PaperComponent={this.paperComponent}
+                    aria-labelledby="draggable-dialog-title"
+                >
+                    <DialogTitle style={{cursor: 'move', alignSelf: "center"}} id="draggable-dialog-title">
+                        Data successfully copied
+                    </DialogTitle>
+                    <DialogContent style={{textAlign: "center"}}>
+                        <DialogContentText>
+                            Now you are ready to paste copied URL into your google calendar.
+                            <br/><br/>Click "Open google calendar" button below to be redirected directly into the
+                            settings.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            OK
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Paper>
         );
     }
 }
@@ -94,7 +95,7 @@ const styles = {
     },
     paper: {
         textAlign: "-webkit-center",
-        marginRight:"10px",
+        marginRight: "10px",
         paddingBottom: "30px"
     },
     logo: {
