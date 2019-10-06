224a11935398fdf08fd43a98f93a2f320d64ae95
SiRazoR <torecz@o2.pl>
2019-10-06 18:50:57 +0200

    Updates

---
+++ precache-manifest.b3b7b58f5d52803e450cfa665a4d0cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8cd52fb75adb6739fb20013c2f0a4d5",
+    "url": "/SiRazoR/calendar-generator/static/media/calendar.c8cd52fb.png"
+  },
+  {
+    "revision": "54035fe675976aeca966",
+    "url": "/SiRazoR/calendar-generator/static/js/runtime~main.59125362.js"
+  },
+  {
+    "revision": "31664737ca048b145d0e",
+    "url": "/SiRazoR/calendar-generator/static/js/main.f1bbe60f.chunk.js"
+  },
+  {
+    "revision": "7f668222cf117e7d03bc",
+    "url": "/SiRazoR/calendar-generator/static/js/2.40fd6a06.chunk.js"
+  },
+  {
+    "revision": "31664737ca048b145d0e",
+    "url": "/SiRazoR/calendar-generator/static/css/main.e96cc822.chunk.css"
+  },
+  {
+    "revision": "7f668222cf117e7d03bc",
+    "url": "/SiRazoR/calendar-generator/static/css/2.7d1aa292.chunk.css"
+  },
+  {
+    "revision": "15285486966ff3b4a4cea80b1222c774",
+    "url": "/SiRazoR/calendar-generator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f5536cce8e7192258792815ec3efb74.js"
+  "/SiRazoR/calendar-generator/precache-manifest.b3b7b58f5d52803e450cfa665a4d0cae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/SiRazoR/calendar-generator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
